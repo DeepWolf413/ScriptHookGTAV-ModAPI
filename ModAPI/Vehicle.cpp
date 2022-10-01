68dc79ae22a156ffc668d7bafa16498f23f7e9fa
DeepWolf413 <jesper15fuji@live.dk>
2022-10-02 00:49:54 +0200

    Added a few more vehicle native wrappers.

--- ModAPI/Vehicle.cpp
+++ ModAPI/Vehicle.cpp
@@ -10,16 +10,31 @@ namespace ModAPI
     void Vehicle::EveryoneLeaveVehicle() const
     { TASK::TASK_EVERYONE_LEAVE_VEHICLE(handle); }
 
-    bool Vehicle::IsSeatFree(const eVehicleSeat seat) const
-    { return VEHICLE::IS_VEHICLE_SEAT_FREE(handle, seat, false); }
+    bool Vehicle::IsDriveable() const
+    {
+        return VEHICLE::IS_VEHICLE_DRIVEABLE(handle, true);
+    }
+
+    bool Vehicle::AreAnySeatsFree() const
+    {
+        return VEHICLE::ARE_ANY_VEHICLE_SEATS_FREE(handle);
+    }
+
+    bool Vehicle::IsSeatFree(const eVehicleSeat seat, bool isTaskRunning) const
+    { return VEHICLE::IS_VEHICLE_SEAT_FREE(handle, seat, isTaskRunning); }
 
     int Vehicle::GetSeatsCount() const
     {
         return VEHICLE::GET_VEHICLE_MODEL_NUMBER_OF_SEATS(GetModel().GetHash());
     }
 
+    int Vehicle::GetPassengersCount(const bool includeDriver, const bool includeDeadOccupants) const
+    {
+        return VEHICLE::GET_VEHICLE_NUMBER_OF_PASSENGERS(handle, includeDriver, includeDeadOccupants);
+    }
+
     Entity Vehicle::GetPedInSeat(const eVehicleSeat seat) const
     {
-        return Entity(VEHICLE::GET_PED_IN_VEHICLE_SEAT(GetHandle(), seat, FALSE));
+        return {VEHICLE::GET_PED_IN_VEHICLE_SEAT(handle, seat, FALSE)};
     }
 }
--- ModAPI/Vehicle.h
+++ ModAPI/Vehicle.h
@@ -11,9 +11,11 @@ namespace ModAPI
 		Vehicle(::Vehicle vehicleHandle);
 
 		void EveryoneLeaveVehicle() const;
-		[[nodiscard]] bool IsSeatFree(eVehicleSeat seat) const;
+		[[nodiscard]] bool IsDriveable() const;
+		[[nodiscard]] bool AreAnySeatsFree() const;
+		[[nodiscard]] bool IsSeatFree(eVehicleSeat seat, bool isTaskRunning = true) const;
 		[[nodiscard]] int GetSeatsCount() const;
-		[[nodiscard]] int GetPassengersCount(bool includeDriver, bool includeDeadOccupants);
+		[[nodiscard]] int GetPassengersCount(bool includeDriver, bool includeDeadOccupants) const;
 		[[nodiscard]] Entity GetPedInSeat(eVehicleSeat seat) const;
 	};
 }
